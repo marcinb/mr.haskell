747fee557cd6972ae7a135c1713cb30beb4b60f3
Marcin Balinski <balinski@gmail.com>
2015-12-04 11:28:27 +0100

    Error gracefully when todo file doesn't exist

--- todo.hs
+++ todo.hs
@@ -2,6 +2,8 @@ import System.Environment
 import Data.List
 import System.Directory
 import System.IO
+import System.IO.Error
+import Control.Exception
 
 dispatch :: [(String, ([String] -> IO ()))]
 dispatch = [ ("add", add)
@@ -14,17 +16,25 @@ main = do
     let (Just action) = lookup command dispatch
     action args
 
+add :: [String] -> IO ()
+add [fileName, task] = appendFile fileName (task ++ "\n")
+
 view :: [String] -> IO ()
 view [fileName] = do
+    catch (_view [fileName]) (readFileExceptionHandler fileName)
+
+_view :: [String] -> IO ()
+_view [fileName] = do
     contents <- readFile fileName
     let numberedTasks = zipWith (\n line -> show n ++ " - " ++ line) [0..] (lines contents)
     putStr (unlines numberedTasks)
 
-add :: [String] -> IO ()
-add [fileName, task] = appendFile fileName (task ++ "\n")
-
 remove :: [String] -> IO ()
 remove [fileName, numberStr] = do
+    catch (_remove [fileName, numberStr]) (readFileExceptionHandler fileName)
+
+_remove :: [String] -> IO ()
+_remove [fileName, numberStr] = do
     handle <- openFile fileName ReadMode
     (tempName, tempHandle) <- openTempFile "." "todo"
     contents <- hGetContents handle
@@ -36,4 +46,9 @@ remove [fileName, numberStr] = do
     hClose tempHandle
     removeFile fileName
     renameFile tempName fileName
+
+readFileExceptionHandler :: String -> IOError -> IO ()  
+readFileExceptionHandler fileName e  
+    | isDoesNotExistError e = putStrLn ("The file " ++ fileName ++ " doesn't exist!")
+    | otherwise = ioError e  
 
